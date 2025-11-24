c8e092eae673e19fb386e51e3cf30f240d03fcb3
thehamzasani <yourEmail@example.com>
2025-11-24 10:22:12 +0500

    Add Sign up api and next auth type

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,53 @@
+// import 'next-auth';
+
+// declare module 'next-auth' {
+//   interface Session {
+//     user: {
+//       _id?: string;
+//       isVerified?: boolean;
+//       isAcceptingMessages?: boolean;
+//       username?: string;
+//     } & DefaultSession['user'];
+//   }
+
+//   interface User {
+//     _id?: string;
+//     isVerified?: boolean;
+//     isAcceptingMessages?: boolean;
+//     username?: string;
+//   }
+// }
+
+// declare module 'next-auth/jwt' {
+//   interface JWT {
+//     _id?: string;
+//     isVerified?: boolean;
+//     isAcceptingMessages?: boolean;
+//     username?: string;
+//   }
+// }
+
+import 'next-auth';
+declare module 'next-auth' {
+    interface Session {
+        user : {
+            _id?: string;
+            isVerified?: boolean;
+            username?: string;
+        } & DefaultSession['user'];
+    }
+
+    interface User {
+        _id?: string;
+        isVerified?: boolean;
+        username?: string;
+    }
+
+    declare module 'next-auth/jwt' {
+        interface JWT {
+            _id?: string;
+            isVerified?: boolean;
+            username?: string;
+        }
+    }
+}
